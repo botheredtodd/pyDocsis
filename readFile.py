6669d70300c4e8cb6a21ed04c55cddfc3cff21b1
botheredtodd <tbals@me.com>
2021-11-03 18:41:33 -0400

    okay, the built in editor is kinda cool

--- readFile.py
+++ readFile.py
@@ -8,3 +8,20 @@ from mib import mib
 from mtaConfig import mtaConfig
 from cmConfig import cmConfig
 
+cm = cmConfig()
+cm.generateStringFromFile(sys.argv[1])
+if len(sys.argv) > 1:
+    if sys.argv[2] == "MTA":
+		cm.tags = MTATlvs
+	#print(cm.tlv_string)
+cm.tlvs = cm.parse(cm.tlv_string, cm.tags)
+cm.encode()
+print("########")
+for t in cm.tlvs:
+	if t.datatype in ["uchar", "uint", "ushort", "hexstr"]:
+			bb = t.getValue()
+			print(bb)
+
+	else:
+		if t.datatype not in  ["snmp_object", "aggregate"]:
+			print("Write a decoder for " + t.datatype)
