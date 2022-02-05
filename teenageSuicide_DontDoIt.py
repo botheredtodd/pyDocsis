2bd6b31f879f46e09b9904c63c0a77298eaa3a79
Todd Balsley <tbals@me.com>
2022-02-05 11:24:31 -0500

    first pass at the suicide prevention hotline update

--- teenageSuicide_DontDoIt.py
+++ teenageSuicide_DontDoIt.py
@@ -16,4 +16,10 @@ for t in cm.tlvs:
 		for line in m.value.split('\n'):
 			print("##### " + line + " #####")
 			newstring.append(line + "\n")
-			
+			if line == '"[2-8]11" : RETURN(#0)':
+				newstring.append('"988" : RETURN\n')
+			elif ')S" : MAKE-CALL("sip:" #1 =domain =dialPhone)' in line:
+				newstring.append('"(988)" : MAKE-CALL(sip: #1 =domain =dialPhone)\n')
+	t.setValue(newstring)
+cm.configFilePath += ".new"
+cm.encode()
