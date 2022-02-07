b3dd04e206bd57a8f7341a02813976e859a90b6c
Todd Balsley <tbals@me.com>
2022-02-07 13:13:16 -0500

    added some comments

--- teenageSuicide_DontDoIt.py
+++ teenageSuicide_DontDoIt.py
@@ -3,6 +3,7 @@ from mtaConfig import mtaConfig
 import sys 
 import binascii
 
+# The bats that load a config file:
 cm = mtaConfig()
 cm.generateStringFromFile(sys.argv[1])
 cm.tags = MTATlvs
@@ -10,18 +11,19 @@ print(cm.tlv_string)
 cm.tlvs = cm.parse(cm.tlv_string, cm.tags)
 
 for t in cm.tlvs:
+	#By default, the tlv value is left in hex as a string. getValue() decodes it.
 	t.getValue()
-	#print(t.value)
-	#oldvalue = ""
-	if t.tag =="64":
-		newstring = "0F"
+
+	if t.tag =="64": #In addition to being stupid, dialmaps are fat. This means they end up in TLV64 rather than TLV11
+		newstring = "0F" # this is the ASCII "shift in" character, which seems to start every dialmap
 		m = t.getValue()
 		print(m.oid)
 		print(m.dataType)
 		oldvalue = m.value
-		for line in m.value.split('\r\n'):
-			print("##### " + line + " #####")
+		for line in m.value.split('\r\n'): #CRLF is what I am forcing the line endings into
+			#print("##### " + line + " #####")
 			newstring += binascii.hexlify(line.encode()).decode('UTF-8')#[:]
+			#The next two lines put the CRLF back on, but in the format the file uses.
 			newstring += "0D"
 			newstring += "0A"
 			#newstring + "\r\n"
@@ -34,14 +36,10 @@ for t in cm.tlvs:
 				newstring += "0D"
 				newstring += "0A"
 		#m.value = ""
-		print(newstring)
-		m.value = newstring[:-2]
-		#print("New Value")
-		#print(m.value)
-		#print("Old Value")
-		#print(oldvalue)
+		#print(newstring)
+		m.value = newstring[:-2] #drop the last two bytes, which are a CRLF.
 		t.value = m.encode()
-		m = t.getValue()
+		#m = t.getValue()
 		#print(t.value)
-cm.configFilePath += ".new"
+cm.configFilePath += ".new" #rename so you don't overwrite the original file
 #cm.encode()
