b854f8057a681b1ce8739e7daa0dbc85878a1c5e
Todd Balsley <tbals@me.com>
2022-05-25 18:24:21 -0400

    ..

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyDocsis',
+    version='0.0',
+    packages=['pydocsis'],
+    url='https://github.com/botheredtodd/pyDocsis',
+    license='LGPL-3.0 license',
+    author='Todd Balsley',
+    author_email='',
+    description=''
+)
