3a6757f111f9a9e7a65c1dd289a2ca3abad0a9c5
Todd <botheredtodd@users.noreply.github.com>
2023-06-12 10:17:22 -0400

    looking at some things...

--- setup.py
+++ setup.py
@@ -2,11 +2,30 @@ from setuptools import setup
 
 setup(
     name='pyDocsis',
-    version='0.2',
+    version='0.3',
     packages=['pydocsis'],
     url='https://github.com/botheredtodd/pyDocsis',
     license='LGPL-3.0 license',
     author='Todd Balsley',
     author_email='todd.balsley@cox.com',
     description='A library for working with docsis files'
+    install_requires=[
+        'joblib',
+        'requests',
+        'ipaddress',
+        'urllib3',
+        'six',
+        "sqlalchemy",
+        "sshtunnel",
+        "pymysql",
+        "sqlalchemy-utils",
+        "psycopg",
+
+
+    ],
+    # python_requires='>=3.10',
+    scripts=[
+        'Scripts/cm_decode',
+    ]
 )
+
