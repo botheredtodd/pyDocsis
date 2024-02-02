ea35881984c827b3c46aa66d9eb11d35244eca7a
Todd Balsley <tbals@me.com>
2024-02-02 10:10:12 -0500

    Update setup.py
    updating authors

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='pyDocsis',
     version='0.3.39',
     packages=['pydocsis'],
-    long_description="pyDocsis: A library for doing things to DOCSIS configs with python.  This library is a work in progress and is not yet ready for production use.  It is being developed by Todd Balsley at Cox Communications.",
+    long_description="pyDocsis: A library for doing things to DOCSIS configs with python.  This library is a work in progress and is not yet ready for production use.  It is being developed by Todd Balsley and Yu X at Cox Communications.",
     url='https://github.com/botheredtodd/pyDocsis',
     license='Apache License 2.0',
-    author='Todd Balsley',
+    author='Todd Balsley and Yu X',
     author_email='todd.balsley@cox.com',
     description='A library for working with docsis files',
     install_requires=[
